c88bef0a78b6e0260d6ce580987ab51cd1475592
dracuxan <nisargkhodke@gmail.com>
2025-05-17 23:11:44 +0530

    feat: add encoder, char_len and base64 struct

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,4 @@ const std = @import("std");
 const structs = @import("root.zig");
 const Base64 = structs.Base64;
 
-pub fn main() !void {
-    const bits = 0b10010111;
-    std.debug.print("{d}\n", .{bits & 0b00110000});
-}
+pub fn main() !void {}
